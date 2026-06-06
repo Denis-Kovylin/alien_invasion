cdea3933e0fa15d5696cb1e1dd8b08252e00e028
Denis Kovylin <kovylin8519@gmail.com>
2026-06-06 15:40:12 +0300

    Implement ship movement with speed control and screen boundaries

--- settings.py
+++ settings.py
@@ -8,3 +8,6 @@ class Settings:
         self.screen_width = 1200
         self.screen_height = 800
         self.bg_color = (15, 15, 35)
+
+        # Настройки корабля
+        self.ship_speed = 1.5
--- ship.py
+++ ship.py
@@ -6,6 +6,7 @@ class Ship:
     def __init__(self,ai_game):
         '''Инициализировать каробль и задать его изначальную позицию'''
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
 
         # Загрузить изображения корабля и получить его rect
@@ -14,6 +15,23 @@ class Ship:
 
         # Создать каждый новый корабль внизу экрана по центру
         self.rect.midbottom = self.screen_rect.midbottom
+
+        # Сохранить десятичное значение для позиции корабля по горизонтали
+        self.x = float(self.rect.x)
+
+        # Индикатор движения
+        self.moving_right = False
+        self.moving_left = False
+
+    def update(self):
+        '''Обновить текущую позицию корабля на основе индикатора движения'''
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
+
+        # Обновить объект rect с self.x
+        self.rect.x = self.x
 
     def blitme(self):
         '''Отрисовать корабль в его текущем местоположении'''
